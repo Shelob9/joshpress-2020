ec6152fbf2cc2f2b88ce54eb4f92f1616d52bea8
Josh Pollock <jpollock412@gmail.com>
2020-03-28 17:18:37 -0400

    proxy for legacy posts

--- server/server.js
+++ server/server.js
@@ -1,9 +1,23 @@
-var proxy = require("express-http-proxy");
-var app = require("express")();
-
+const proxy = require("express-http-proxy");
+const app = require("express")();
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 const port = 3000;
-
-app.get("/", (req, res) => res.send("Hello World!"));
+const hasLegacyPost = path => {
+	path = path.replace(/\/?$/, "/");
+	const legacyPosts = require("./legacyPosts");
+	return legacyPosts.includes(path);
+};
+app.use(function(req, res, next) {
+	if (hasLegacyPost(req.path)) {
+		res.redirect(301, `https://joshpress.net${req.path}`);
+	}
+	next();
+});
+app.use("/proxy", proxy("www.google.com"));
+app.get("/a", (req, res) => {
+	return res.json({ hi: "Roy" });
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
-app.use("/proxy", proxy("www.google.com"));
