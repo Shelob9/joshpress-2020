1acb644cfb22f884562003a9998e8f7e1ae6a454
Josh Pollock <jpollock412@gmail.com>
2020-04-02 19:56:36 -0400

    rework express server

--- client/gatsby-config.js
+++ client/gatsby-config.js
@@ -1,3 +1,5 @@
+var proxy = require("http-proxy-middleware")
+
 module.exports = {
   plugins: [
     {
---
+++ server/public/styles-e8e49f195427027559d9.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{U1P7:function(n,o,w){},kqb9:function(n,o,w){}}]);
+//# sourceMappingURL=styles-e8e49f195427027559d9.js.map
