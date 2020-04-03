d8703fb4abbc481c4bb156dee86371b18edf299c
Josh Pollock <jpollock412@gmail.com>
2020-04-02 20:22:21 -0400

    Work on front page words

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,28 +1,10 @@
 import React from "react"
 import { css, Styled } from "theme-ui"
 import Header from "gatsby-theme-blog/src/components/header"
-
-const Section = ({ url, about, title, linkText, years }) => (
-  <React.Fragment key={url}>
-    <Styled.h3>{title}</Styled.h3>
-    <Styled.p>{about}</Styled.p>
-    <Styled.ul>
-      {years && <Styled.li>{years}</Styled.li>}
-      {url ? (
-        <Styled.li>
-          <Styled.a href={url}>{linkText}</Styled.a>
-        </Styled.li>
-      ) : (
-        <Styled.p>{linkText}</Styled.p>
-      )}
-    </Styled.ul>
-  </React.Fragment>
-)
 export default ({ children, ...props }) => (
   <Styled.root>
     <div>
       <Header title={"Josh Pollock"} {...props} />
-
       <div
         css={css({
           maxWidth: `container`,
@@ -31,68 +13,82 @@ export default ({ children, ...props }) => (
           py: 4,
         })}
       >
-        <Styled.p>Initial words</Styled.p>
-        <Styled.h2>Work</Styled.h2>
-        <section
-          css={css({
-            mx: `12`,
-          })}
-        >
-          {[
-            {
-              title: "Saturday Drive: VP Of Engineering Experience",
-              about:
-                "Currently, I lead the engineering team that is responsible for the WordPress plugins Ninja Forms, Caldera Forms and SendWP.",
-              url: "https://saturdaydrive.com",
-              linkText: "Saturday Drive",
-              years: "2019-present",
-            },
-            {
-              title: "CalderaWP: Lead Developer/ Co-Owner",
-              about:
-                "I co-founded CalderaWP, lead development of the plugin Caldera Forms, as well as other plugins and web apps. I also oversaw",
-              url: "https://CalderaForms.com",
-              linkText: "CalderaWP",
-              years: "2015-2019",
-            },
-            {
-              title: "Pods: Junior Developer/ Community Manager",
-              about: "Words",
-              url: "https://pods.io",
-              linkText: "Pods",
-              years: "2014-2016",
-            },
-          ].map(p => (
-            <Section key={p.url} {...p} />
-          ))}
-        </section>
-        <Styled.h2>Writing</Styled.h2>
+        <Styled.p>Hi.</Styled.p>
         <section>
-          {[
-            {
-              title: "Torque Mag: A WPEngine Publication",
-              about:
-                "For many years I wrote about WordPress development and community for WPEngine's community site.",
-              url: "https://torquemag.io/author/joshp/",
-              linkText: "Torque",
-            },
-            {
-              title: "DEV",
-              about:
-                "Recently I've been sharing short posts about JavaScript, TypeScript, PHP and WordPress development on dev.to",
-              url: "https://dev.to/shelob9",
-              linkText: "My Dev Posts",
-            },
-            {
-              title: "Envato Tuts+",
-              about:
-                "I wrote about WordPress development for Tuts+ many years ago.",
-              url: "https://tutsplus.com/authors/josh-pollock",
-              linkText: "My Tuts+ Posts",
-            },
-          ].map(p => (
-            <Section key={p.url} {...p} />
-          ))}
+          <Styled.p>
+            {" "}
+            Praesent a urna sit amet erat pretium euismod. Integer hendrerit
+            mollis diam, faucibus facilisis arcu dignissim non. Duis euismod ex
+            arcu, quis mattis nibh consequat vitae. Cras ultricies, turpis ut
+            pulvinar dictum,{" "}
+          </Styled.p>
+        </section>
+        <section>
+          <Styled.h2>Work</Styled.h2>
+
+          <dl>
+            <dt>Saturday Drive: VP Of Engineering Experience</dt>
+            <dd>
+              Enginering team lead, helping build Ninja Forms and Caldera Forms
+              WordPress plugins and tranistion to remote work.
+            </dd>
+            <dd>2019-Present</dd>
+            <dt>CalderaWP: Co-Owner/ Lead Developer</dt>
+            <dd>...</dd>
+            <dd>Founded: 2015. Aquired: 2019</dd>
+            <dt>Pods Framework: Community Manager/ Developer</dt>
+            <dd>
+              Developed features, fixed bugs, wrote documentation and offered
+              support for the free WordPress plugin Pods. Also, created a
+              community fundraising program to fund the project.
+            </dd>
+            <dd>2014-2015</dd>
+          </dl>
+        </section>
+        <section>
+          <Styled.h2>Writing</Styled.h2>
+
+          <dl>
+            <dt>Torque Mag: A WPEngine Publication</dt>
+            <dd>
+              For many years I wrote about WordPress development and community
+              for WPEngine's community site.
+            </dd>
+            <dd>
+              <a href="https://torquemag.io/author/joshp/">Torque</a>
+            </dd>
+            <dt>DEV</dt>
+            <dd>
+              Recently I've been sharing short posts about JavaScript,
+              TypeScript, PHP and WordPress development on dev.to
+            </dd>
+            <dd>
+              <a href="https://dev.to/shelob9">My Dev Posts</a>
+            </dd>
+            <dt>Envato Tuts+</dt>
+            <dd>
+              I wrote about WordPress development for Tuts+ many years ago.
+            </dd>
+            <dd>
+              <a href="https://tutsplus.com/authors/josh-pollock">
+                My Tuts+ Posts
+              </a>
+            </dd>
+          </dl>
+        </section>
+        <section>
+          <Styled.h2>Social Media</Styled.h2>
+          <ul>
+            <li>
+              <a href="https://twitter.com/josh412">Twitter</a>
+            </li>
+            <li>
+              <a href="https://github.com/shelob9">Githun</a>
+            </li>
+            <li>
+              <a href="https://www.instagram.com/pollockjosh/">Instagram</a>
+            </li>
+          </ul>
         </section>
       </div>
     </div>
